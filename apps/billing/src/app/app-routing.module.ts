a1e553b9d33ede7e7b129d2c96862c38deb5f592
Jorge Dacosta <jorgeMSDC@mail.com>
2019-12-04 12:54:59 +0100

    lazy-load contacts module

--- apps/billing/src/app/app-routing.module.ts
+++ apps/billing/src/app/app-routing.module.ts
@@ -2,7 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'contacts',
+    loadChildren: () => import('@contacts/contacts.module').then(m => m.ContactsModule)
+  }
+];
 
 @NgModule({
   imports: [BrowserModule, RouterModule.forRoot(routes)],
