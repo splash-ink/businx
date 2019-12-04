9e9a21d8d85d076bb6db24389f8bb8711726b529
Jorge Dacosta <jorgeMSDC@mail.com>
2019-12-04 14:58:06 +0100

    Add card-overview widget

---
+++ apps/billing/src/app/contacts/shared/card-overview/card-overview.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'businx-card-overview',
+  templateUrl: './card-overview.component.html',
+  styleUrls: ['./card-overview.component.css']
+})
+export class CardOverviewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
