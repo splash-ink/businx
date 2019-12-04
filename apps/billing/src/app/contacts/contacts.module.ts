06126df766a0ddcd96e80704c6cee872b276ce65
Jorge Dacosta <jorgeMSDC@mail.com>
2019-12-04 16:17:25 +0100

    Importing shared feat. component

--- apps/billing/src/app/contacts/contacts.module.ts
+++ apps/billing/src/app/contacts/contacts.module.ts
@@ -5,13 +5,15 @@ import { ContactsRoutingModule } from './contacts-routing.module';
 import { PageTitleComponent } from './shared/page-title/page-title.component';
 import { ContactsComponent } from './contacts.component';
 import { CardOverviewComponent } from './shared/card-overview/card-overview.component';
+import { ListComponent } from './share/list/list.component';
 
 
 @NgModule({
   declarations: [
     ContactsComponent,
     PageTitleComponent,
-    CardOverviewComponent
+    CardOverviewComponent,
+    ListComponent
   ],
   imports: [
     CommonModule,
