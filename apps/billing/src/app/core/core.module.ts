3fa6b66d198be086328ead99c21c613b948a5014
Jorge Dacosta <jorgeMSDC@mail.com>
2019-12-05 23:42:36 +0100

    fix component name

--- apps/billing/src/app/core/core.module.ts
+++ apps/billing/src/app/core/core.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavComponent } from './nav/nav.component';
-import { SidebarComponent } from './sidebar/sidebar.component';
+import { SideNavComponent } from './sidenav/sidenav.component';
 
 const comps = [
   NavComponent,
-  SidebarComponent
+  SideNavComponent
 ];
 
 
