d37fecc91b7e275d47c208f0e71735140a19ba91
Jorge Dacosta <jorgeMSDC@mail.com>
2019-12-05 23:37:48 +0100

    declaring & exporting sidebar & nav components

--- apps/billing/src/app/core/core.module.ts
+++ apps/billing/src/app/core/core.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavComponent } from './nav/nav.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
 
+const comps = [
+  NavComponent,
+  SidebarComponent
+];
 
 
 @NgModule({
-  declarations: [NavComponent],
-  exports: [NavComponent],
+  declarations: comps,
+  exports: comps,
   imports: [
     CommonModule
   ]
