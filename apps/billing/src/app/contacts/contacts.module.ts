f5ddc872b7f98447cc63249fc174e2cb2523a56a
Jorge Dacosta <jorgeMSDC@mail.com>
2019-12-06 14:34:09 +0100

    shift out unavailable comps

--- apps/billing/src/app/contacts/contacts.module.ts
+++ apps/billing/src/app/contacts/contacts.module.ts
@@ -3,17 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { ContactsRoutingModule } from './contacts-routing.module';
 import { ContactsComponent } from './contacts.component';
-import { PageTitleComponent } from './shared/page-title/page-title.component';
-import { CardOverviewComponent } from './shared/card-overview/card-overview.component';
-import { ContactListComponent } from './shared/contact-list/contact-list.component';
+import { CardOverviewComponent } from '../shared/card-overview/card-overview.component';
 
 
 @NgModule({
   declarations: [
     ContactsComponent,
-    PageTitleComponent,
     CardOverviewComponent,
-    ContactListComponent
   ],
   imports: [
     CommonModule,
