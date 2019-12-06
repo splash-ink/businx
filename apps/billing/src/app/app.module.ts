0dcff1fb131485f6508fa290bba2cf2c30bb70de
Jorge Dacosta <jorgeMSDC@mail.com>
2019-12-06 14:28:59 +0100

    import shared module

--- apps/billing/src/app/app.module.ts
+++ apps/billing/src/app/app.module.ts
@@ -3,12 +3,14 @@ import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 import { CoreModule } from '@core/core.module';
+import { SharedModule } from '@shared/shared.module';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
     AppRoutingModule,
-    CoreModule
+    CoreModule,
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
