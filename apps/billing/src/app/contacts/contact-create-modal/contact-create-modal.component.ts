6a27e1430427f36bbdd4a9fd47cab50bdc234b19
Jorge Dacosta <jorgeMSDC@mail.com>
2019-12-10 23:16:36 +0100

    Setup forms & apply claims

--- apps/billing/src/app/contacts/contact-create-modal/contact-create-modal.component.ts
+++ apps/billing/src/app/contacts/contact-create-modal/contact-create-modal.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators as _ } from '@angular/forms';
 
 @Component({
   selector: 'businx-contact-create-modal',
@@ -13,6 +13,15 @@ export class ContactCreateModalComponent implements OnInit {
   constructor(private fb: FormBuilder) { }
 
   ngOnInit() {
+    this.form = this.fb.group({
+      accountType: ['', [ _.required ]],
+      greet: ['', [ _.required ]],
+      name: ['', [ _.required, _.minLength(5), _.maxLength(46) ]],
+      phone: ['', [ _.minLength(7), _.maxLength(14) ]],
+      email: ['', [ _.email ]],
+      nif: [''],
+      coin: ['', [ _.required ]]
+    });
   }
 
 }
