75deadd4c29975684b33701148aeeaf1c9fe991c
Jorge Dacosta <jorgeMSDC@mail.com>
2019-12-11 00:02:18 +0100

    import globals & setup helpers for selt ops
    add company input field to form

--- apps/billing/src/app/contacts/contact-create-modal/contact-create-modal.component.ts
+++ apps/billing/src/app/contacts/contact-create-modal/contact-create-modal.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators as _ } from '@angular/forms';
+import { COINS, GREETINGS, Coins } from '@businx/data-models';
 
 @Component({
   selector: 'businx-contact-create-modal',
@@ -9,6 +10,9 @@ import { FormGroup, FormBuilder, Validators as _ } from '@angular/forms';
 export class ContactCreateModalComponent implements OnInit {
 
   form: FormGroup;
+
+  coins: Coins [];
+  greetings;
   
   constructor(private fb: FormBuilder) { }
 
@@ -17,11 +21,15 @@ export class ContactCreateModalComponent implements OnInit {
       accountType: ['', [ _.required ]],
       greet: ['', [ _.required ]],
       name: ['', [ _.required, _.minLength(5), _.maxLength(46) ]],
+      company: ['', [ _.maxLength(2) ]],
       phone: ['', [ _.minLength(7), _.maxLength(14) ]],
       email: ['', [ _.email ]],
       nif: [''],
       coin: ['', [ _.required ]]
     });
+
+    this.coins = COINS;
+    this.greetings = GREETINGS;
   }
 
 }
