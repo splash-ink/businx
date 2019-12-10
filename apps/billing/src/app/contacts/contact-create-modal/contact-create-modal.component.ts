59e9e5227004a557f54dbb4718ae9ff1f288fdaf
Jorge Dacosta <jorgeMSDC@mail.com>
2019-12-10 15:36:52 +0100

    Add Modal to create new Contact

---
+++ apps/billing/src/app/contacts/contact-create-modal/contact-create-modal.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'businx-contact-create-modal',
+  templateUrl: './contact-create-modal.component.html',
+  styleUrls: ['./contact-create-modal.component.css']
+})
+export class ContactCreateModalComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
