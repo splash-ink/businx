ea15d150377eaa15982202947e1578f66aa4a939
Jorge Dacosta <jorgeMSDC@mail.com>
2019-12-11 22:31:28 +0100

    Add Details page for contacts

---
+++ apps/billing/src/app/contacts/contact-details/contact-details.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'businx-contact-details',
+  templateUrl: './contact-details.component.html',
+  styleUrls: ['./contact-details.component.css']
+})
+export class ContactDetailsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
