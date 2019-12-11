cc518b93a70fac9712a2f4d58f9313b6ed67b151
Jorge Dacosta <jorgeMSDC@mail.com>
2019-12-11 22:31:15 +0100

    Add details as part of module

--- apps/billing/src/app/contacts/contacts.module.ts
+++ apps/billing/src/app/contacts/contacts.module.ts
@@ -5,12 +5,14 @@ import { ContactsRoutingModule } from './contacts-routing.module';
 import { ContactsComponent } from './contacts.component';
 import { SharedModule } from '@bill/shared/shared.module';
 import { ContactCreateModalComponent } from './contact-create-modal/contact-create-modal.component';
+import { ContactDetailsComponent } from './contact-details/contact-details.component';
 
 
 @NgModule({
   declarations: [
     ContactsComponent,
     ContactCreateModalComponent,
+    ContactDetailsComponent,
   ],
   imports: [
     CommonModule,
