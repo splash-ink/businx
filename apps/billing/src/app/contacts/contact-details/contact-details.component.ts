57a130a47e07a2bd5f2eaf115ab2604521929dc4
Jorge Dacosta <jorgeMSDC@mail.com>
2019-12-15 20:31:37 +0100

    DI for ActivatedRoute;
    Retrieving URL param id;
    
    Setup invoices table for current contact;

--- apps/billing/src/app/contacts/contact-details/contact-details.component.ts
+++ apps/billing/src/app/contacts/contact-details/contact-details.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Table } from '@bill/shared/table/table';
+import { ActivatedRoute } from '@angular/router';
+import { switchMap } from 'rxjs/operators';
 @Component({
   selector: 'businx-contact-details',
   templateUrl: './contact-details.component.html',
@@ -7,9 +9,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactDetailsComponent implements OnInit {
 
-  constructor() { }
+  invoicesTbl: Table;
+  id;
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit() {
+    this.route
+    .paramMap
+    .pipe(
+      switchMap(params => this.id = params.get('id'))
+    );
+
+    this.invoicesTbl = {
+      columns: ['Fatura', 'Ordem', 'Valor', 'Impostos'],
+      dataset: [],
+      collection: `${this.id}/invoices`
+    };
   }
 
 }
