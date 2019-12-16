521082c69205358b2eef56551060d0a9214619e5
Jorge Dacosta <jorgeMSDC@mail.com>
2019-12-16 14:11:58 +0100

    New Contact-Form Component

---
+++ apps/billing/src/app/contacts/shared/contact-form/contact-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'businx-contact-form',
+  templateUrl: './contact-form.component.html',
+  styles: []
+})
+export class ContactFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
