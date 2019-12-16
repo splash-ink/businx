d633259a0b42533d559ad0fe3e7ce8aef3d10bc4
Jorge Dacosta <jorgeMSDC@mail.com>
2019-12-16 18:19:34 +0100

    Initial business logic for contact form

--- apps/billing/src/app/contacts/shared/contact-form/contact-form.component.ts
+++ apps/billing/src/app/contacts/shared/contact-form/contact-form.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, Input } from '@angular/core';
+import { FormGroup, FormBuilder, Validators as _ } from '@angular/forms';
+import { COINS, GREETINGS, Coins } from '@businx/data-models';
 @Component({
   selector: 'businx-contact-form',
   templateUrl: './contact-form.component.html',
@@ -7,9 +8,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactFormComponent implements OnInit {
 
-  constructor() { }
+  @Input() type: string;
+  
+  form: FormGroup;
+
+  coins: Coins [];
+  greetings;
+
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit() {
+    this.form = this.fb.group({
+      accountType: ['', [ _.required ]],
+      greet: ['', [ _.required ]],
+      name: ['', [ _.required, _.minLength(5), _.maxLength(46) ]],
+      company: ['', [ _.maxLength(2) ]],
+      phone: ['', [ _.minLength(7), _.maxLength(14) ]],
+      email: ['', [ _.email ]],
+      nif: [''],
+      coin: ['', [ _.required ]]
+    });
+
+    this.coins = COINS;
+    this.greetings = GREETINGS;
   }
 
 }
