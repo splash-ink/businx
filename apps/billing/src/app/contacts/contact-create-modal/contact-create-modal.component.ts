83b1e8acda6837c81047965961aee31338600922
Jorge Dacosta <jorgeMSDC@mail.com>
2019-12-16 18:17:50 +0100

    Remove form setup config

--- apps/billing/src/app/contacts/contact-create-modal/contact-create-modal.component.ts
+++ apps/billing/src/app/contacts/contact-create-modal/contact-create-modal.component.ts
@@ -1,6 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder, Validators as _ } from '@angular/forms';
-import { COINS, GREETINGS, Coins } from '@businx/data-models';
 
 @Component({
   selector: 'businx-contact-create-modal',
@@ -9,27 +7,11 @@ import { COINS, GREETINGS, Coins } from '@businx/data-models';
 })
 export class ContactCreateModalComponent implements OnInit {
 
-  form: FormGroup;
-
-  coins: Coins [];
-  greetings;
+ 
   
-  constructor(private fb: FormBuilder) { }
+  constructor() { }
 
   ngOnInit() {
-    this.form = this.fb.group({
-      accountType: ['', [ _.required ]],
-      greet: ['', [ _.required ]],
-      name: ['', [ _.required, _.minLength(5), _.maxLength(46) ]],
-      company: ['', [ _.maxLength(2) ]],
-      phone: ['', [ _.minLength(7), _.maxLength(14) ]],
-      email: ['', [ _.email ]],
-      nif: [''],
-      coin: ['', [ _.required ]]
-    });
-
-    this.coins = COINS;
-    this.greetings = GREETINGS;
   }
 
 }
