0b10f308a89d8455f6c7fc0efd8e5860775e77da
Jorge Dacosta <jorgeMSDC@mail.com>
2019-12-16 18:52:56 +0100

    seeding inputs if data exists

--- apps/billing/src/app/contacts/shared/contact-form/contact-form.component.ts
+++ apps/billing/src/app/contacts/shared/contact-form/contact-form.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { FormGroup, FormBuilder, Validators as _ } from '@angular/forms';
-import { COINS, GREETINGS, Coins } from '@businx/data-models';
+import { COINS, GREETINGS, Coins, Contacts } from '@businx/data-models';
 @Component({
   selector: 'businx-contact-form',
   templateUrl: './contact-form.component.html',
@@ -9,6 +9,7 @@ import { COINS, GREETINGS, Coins } from '@businx/data-models';
 export class ContactFormComponent implements OnInit {
 
   @Input() type: string;
+  @Input() incoming?: Contacts;
   
   form: FormGroup;
 
@@ -18,15 +19,26 @@ export class ContactFormComponent implements OnInit {
   constructor(private fb: FormBuilder) { }
 
   ngOnInit() {
-    this.form = this.fb.group({
-      accountType: ['', [ _.required ]],
+    this.form = !this.incoming
+    ? this.fb.group({
+      accountType: ['' , [ _.required ]],
       greet: ['', [ _.required ]],
       name: ['', [ _.required, _.minLength(5), _.maxLength(46) ]],
-      company: ['', [ _.maxLength(2) ]],
+      company: ['', [ _.minLength(2) ]],
       phone: ['', [ _.minLength(7), _.maxLength(14) ]],
       email: ['', [ _.email ]],
       nif: [''],
       coin: ['', [ _.required ]]
+    })
+    : this.fb.group({
+      accountType: [this.incoming.accountType , [ _.required ]],
+      greet: [this.incoming.greet, [ _.required ]],
+      name: [this.incoming.name, [ _.required, _.minLength(5), _.maxLength(46) ]],
+      company: [this.incoming.company, [ _.minLength(2) ]],
+      phone: [this.incoming.phone, [ _.minLength(7), _.maxLength(14) ]],
+      email: [this.incoming.email, [ _.email ]],
+      nif: [this.incoming.nif],
+      coin: [this.incoming.coin, [ _.required ]]
     });
 
     this.coins = COINS;
