fc8318df2d57daff2a44c31f04046bfa082cf561
Jorge Dacosta <jorgeMSDC@mail.com>
2019-12-16 18:53:26 +0100

    add demo contact obj

--- apps/billing/src/app/contacts/contact-edit/contact-edit.component.ts
+++ apps/billing/src/app/contacts/contact-edit/contact-edit.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Contacts } from '@businx/data-models';
 
 @Component({
   selector: 'businx-contact-edit',
@@ -6,6 +7,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contact-edit.component.css']
 })
 export class ContactEditComponent implements OnInit {
+
+  demo: Contacts = {
+    accountType: 'Pessoal',
+    greet: 'Sra.',
+    name: 'Anisa Makayla',
+    company: 'Spatial LLC',
+    email: 'm.anisa@spatial.io',
+    phone: '+1693555021',
+    coin: 'USD',
+    nif: '0328CA852-09'
+  };
 
   constructor() { }
 
