69846d0c3e2989a185f29e81b003d948b359c925
Jorge Dacosta <jorgeMSDC@mail.com>
2019-12-18 12:58:49 +0100

    registing main route path for items

--- apps/billing/src/app/items/items-routing.module.ts
+++ apps/billing/src/app/items/items-routing.module.ts
@@ -1,8 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ItemsComponent } from './items.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    children: [
+      {
+        path: '',
+        component: ItemsComponent
+      }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
