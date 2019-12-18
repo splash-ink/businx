c0628579a5e29a7dae99db5ed48d19ad64b6a803
Jorge Dacosta <jorgeMSDC@mail.com>
2019-12-18 12:33:10 +0100

    declaring items.comp as part of the module

--- apps/billing/src/app/items/items.module.ts
+++ apps/billing/src/app/items/items.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ItemsRoutingModule } from './items-routing.module';
+import { ItemsComponent } from './items.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [ItemsComponent],
   imports: [
     CommonModule,
     ItemsRoutingModule
