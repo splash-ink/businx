5594d0eca93e8842f10acce044d8abba6bf9160a
Jorge Dacosta <jorgeMSDC@mail.com>
2019-12-20 18:03:34 +0100

    ci tests for dynamic table

--- apps/billing/src/app/shared/table/table.component.spec.ts
+++ apps/billing/src/app/shared/table/table.component.spec.ts
@@ -1,14 +1,25 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { TableComponent } from './table.component';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { RouterTestingModule } from '@angular/router/testing';
 
-describe('ContactTableComponent', () => {
+describe('Table Widget', () => {
   let component: TableComponent;
   let fixture: ComponentFixture<TableComponent>;
+  const columns = ['A', 'B', 'C', 'D'];
+  const tableConfig = {
+    collection: '',
+    columns: columns,
+    dataset: [],
+    title: 'Testing'
+  };
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ TableComponent ]
+      declarations: [ TableComponent ],
+      imports: [RouterTestingModule],
+      schemas: [NO_ERRORS_SCHEMA]
     })
     .compileComponents();
   }));
@@ -16,10 +27,59 @@ describe('ContactTableComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(TableComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should abbreviate the name in two', () => {
+    const abbrName = component.abbreviateName('Joe Eames');
+
+    expect(abbrName).toEqual('JE');
   });
+
+  it('should have the corrects columns', () => {
+
+    component.config = tableConfig;
+
+    expect(component.config.columns).toEqual(columns);
+  });
+
+  it('should have item search input', () => {
+    expect(fixture.nativeElement.querySelector('input')).toBeTruthy();    
+  });
+
+  it('should render the apropriate card title', () => {
+    component.config = tableConfig;
+
+    fixture.detectChanges();
+    const el = fixture.nativeElement.querySelector('h6');
+
+    expect(el.textContent).toContain(tableConfig.title);
+  });
+
+  it('should notify when the table is empty', () => {
+    component.config = tableConfig;
+
+    fixture.detectChanges();
+    const el = fixture.nativeElement.querySelector('#emptyTbl');
+
+    expect(el.textContent).toContain(`Sem ${tableConfig.title.toLowerCase()} a mostrar, tabela vazia.`);
+  });
+
+  it('should fill the table with right items', () => {
+    component.config = tableConfig;
+
+    component.config.dataset = [
+      {
+        a: 'A is a field value',
+        b: 'B is a field value',
+        c: 'C is a field value',
+        d: 'D is a field value'
+      }
+    ]
+
+    fixture.detectChanges();
+    const el = fixture.nativeElement.querySelector('td');
+
+    expect(el.textContent).toContain(component.config.dataset[0]['a']);
+  });
+
 });
