5679b4b1d71f7e9adfb859f275f5abcc4823fb8e
Jorge Dacosta <jorgeMSDC@mail.com>
2019-12-23 18:28:55 +0100

    add shared form for items

---
+++ apps/billing/src/app/items/shared/item-form/item-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'businx-item-form',
+  templateUrl: './item-form.component.html',
+  styleUrls: ['./item-form.component.css']
+})
+export class ItemFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
