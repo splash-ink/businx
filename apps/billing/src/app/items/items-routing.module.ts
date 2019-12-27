5965de8c60f4c5fa2c2db42e9e38273c0cf32ffe
Jorge Dacosta <jorgeMSDC@mail.com>
2019-12-27 18:55:35 +0100

    Setup details page route path

--- apps/billing/src/app/items/items-routing.module.ts
+++ apps/billing/src/app/items/items-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ItemsComponent } from './items.component';
+import { ItemDetailsComponent } from './item-details/item-details.component';
 
 
 const routes: Routes = [
@@ -10,6 +11,10 @@ const routes: Routes = [
       {
         path: '',
         component: ItemsComponent
+      },
+      {
+        path: ':id',
+        component: ItemDetailsComponent
       }
     ]
   }
