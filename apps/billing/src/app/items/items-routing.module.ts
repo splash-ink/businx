4480bbfa3d372196c3e80bd10230163de372d799
Jorge Dacosta <jorgeMSDC@mail.com>
2019-12-31 21:31:38 +0100

    Register edit page route path

--- apps/billing/src/app/items/items-routing.module.ts
+++ apps/billing/src/app/items/items-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ItemsComponent } from './items.component';
 import { ItemDetailsComponent } from './item-details/item-details.component';
+import { ItemEditComponent } from './item-edit/item-edit.component';
 
 
 const routes: Routes = [
@@ -15,6 +16,10 @@ const routes: Routes = [
       {
         path: ':id',
         component: ItemDetailsComponent
+      },
+      {
+        path: ':id/edit',
+        component: ItemEditComponent
       }
     ]
   }
