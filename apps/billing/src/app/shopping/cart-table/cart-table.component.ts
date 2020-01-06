648aaa0f75ecfb7c4af2fbbb90d6f6224f882af7
Jorge Dacosta <jorgeMSDC@mail.com>
2020-01-06 20:23:46 +0100

    chore(shopping/feat): add cart table component

---
+++ apps/billing/src/app/shopping/cart-table/cart-table.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'businx-cart-table',
+  templateUrl: './cart-table.component.html',
+  styles: []
+})
+export class CartTableComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
