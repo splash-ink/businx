e983da44659c4bbd68fd61fc9f690e5bc9d03732
Jorge Dacosta <jorgeMSDC@mail.com>
2020-01-06 20:32:41 +0100

    chore(shopping/feat): add summary component for cart

--- apps/billing/src/app/shopping/shopping.module.ts
+++ apps/billing/src/app/shopping/shopping.module.ts
@@ -4,10 +4,11 @@ import { CommonModule } from '@angular/common';
 import { ShoppingRoutingModule } from './shopping-routing.module';
 import { CartComponent } from './cart/cart.component';
 import { CartTableComponent } from './cart-table/cart-table.component';
+import { SummaryComponent } from './summary/summary.component';
 
 
 @NgModule({
-  declarations: [CartComponent, CartTableComponent],
+  declarations: [CartComponent, CartTableComponent, SummaryComponent],
   imports: [
     CommonModule,
     ShoppingRoutingModule
