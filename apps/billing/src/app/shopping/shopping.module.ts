a6130c4625daa9c80d95881bc1a22ac48e4cee28
Jorge Dacosta <jorgeMSDC@mail.com>
2020-01-06 19:43:46 +0100

    chore(shopping/feat): creating cart page

--- apps/billing/src/app/shopping/shopping.module.ts
+++ apps/billing/src/app/shopping/shopping.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ShoppingRoutingModule } from './shopping-routing.module';
+import { CartComponent } from './cart/cart.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [CartComponent],
   imports: [
     CommonModule,
     ShoppingRoutingModule
