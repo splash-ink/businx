8ce9bc84e663cfe286b13e299dd49d4d8b20bfb5
Jorge Dacosta <jorgeMSDC@mail.com>
2020-01-06 20:24:38 +0100

    chore(shopping/feat): register cart-table as sibbling

--- apps/billing/src/app/shopping/shopping.module.ts
+++ apps/billing/src/app/shopping/shopping.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 
 import { ShoppingRoutingModule } from './shopping-routing.module';
 import { CartComponent } from './cart/cart.component';
+import { CartTableComponent } from './cart-table/cart-table.component';
 
 
 @NgModule({
-  declarations: [CartComponent],
+  declarations: [CartComponent, CartTableComponent],
   imports: [
     CommonModule,
     ShoppingRoutingModule
