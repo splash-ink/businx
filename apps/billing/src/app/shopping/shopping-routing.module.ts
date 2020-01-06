0c649236909efd20893c1ee912fdfe5ac17fe8ca
Jorge Dacosta <jorgeMSDC@mail.com>
2020-01-06 19:47:44 +0100

    chore(common/routing/feat): register cart route path

--- apps/billing/src/app/shopping/shopping-routing.module.ts
+++ apps/billing/src/app/shopping/shopping-routing.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CartComponent } from './cart/cart.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    children: [
+      { path: 'cart', component: CartComponent }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
