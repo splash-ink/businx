e19ed2710ab84fe334a9e8c11fe802e02a395454
Jorge Dacosta <jorgeMSDC@mail.com>
2020-01-07 22:13:33 +0100

    chore(shopping/feat): add modal comp to display all items

---
+++ apps/billing/src/app/shopping/modal-item-list/modal-item-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'businx-modal-item-list',
+  templateUrl: './modal-item-list.component.html',
+  styles: []
+})
+export class ModalItemListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
