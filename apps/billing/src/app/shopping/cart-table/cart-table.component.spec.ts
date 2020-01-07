cd4df03f021bd6d53a6c421572c40074b2284422
Jorge Dacosta <jorgeMSDC@mail.com>
2020-01-07 21:43:39 +0100

    chore(testing): check if it has D right numbers of cols
    check if exists an actions-menu

--- apps/billing/src/app/shopping/cart-table/cart-table.component.spec.ts
+++ apps/billing/src/app/shopping/cart-table/cart-table.component.spec.ts
@@ -1,6 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { CartTableComponent } from './cart-table.component';
+import { By } from '@angular/platform-browser';
 
 describe('CartTableComponent', () => {
   let component: CartTableComponent;
@@ -19,7 +20,18 @@ describe('CartTableComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('has five (5) head columns', () => {
+    const th = fixture.debugElement.queryAll(By.css('thead > tr > th'));
+    
     expect(component).toBeTruthy();
+    expect(th.length).toBe(5);
+  });
+
+  it('has a actions-menu that helps manage items inside the cart', () => {
+    const a = fixture.debugElement.queryAll(By.css('.actions > a'));
+
+    expect(a.length).toBe(2);
+    expect(a[0].nativeElement.innerHTML).toBe(`<i class="far fa-external-link-alt"></i>`);
+    expect(a[1].nativeElement.innerHTML).toBe(`<i class="far fa-times"></i>`);
   });
 });
