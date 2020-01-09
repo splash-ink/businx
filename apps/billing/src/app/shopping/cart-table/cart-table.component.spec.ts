c642b7e947182d9d9e079bb3041eb3ded5d190f1
Jorge Dacosta <jorgeMSDC@mail.com>
2020-01-09 19:26:42 +0100

    chore(testing): seed input with dummy data to make test pass;
    new test to check if the btn is removing item safely

--- apps/billing/src/app/shopping/cart-table/cart-table.component.spec.ts
+++ apps/billing/src/app/shopping/cart-table/cart-table.component.spec.ts
@@ -2,10 +2,23 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { CartTableComponent } from './cart-table.component';
 import { By } from '@angular/platform-browser';
+import { CartItem } from '@businx/data-models';
 
 describe('CartTableComponent', () => {
   let component: CartTableComponent;
   let fixture: ComponentFixture<CartTableComponent>;
+  const demo: CartItem = {
+    id: 1,
+    item:{
+      id: 'zyx',
+      service: 'service name',
+      description: 'Loren ipsum, dolet amet rusir.',
+      price: 5000,
+      type: 'tech',
+      buyInfo: false
+    },
+    quantity: 1,
+  }
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -38,10 +51,16 @@ describe('CartTableComponent', () => {
   });
 
   it('has a actions-menu that helps manage items inside the cart', () => {
+    component.docs = [demo];
+
+    fixture.detectChanges();
+
     const a = fixture.debugElement.queryAll(By.css('.actions > a'));
 
     expect(a.length).toBe(2);
     expect(a[0].nativeElement.innerHTML).toBe(`<i class="far fa-external-link-alt"></i>`);
     expect(a[1].nativeElement.innerHTML).toBe(`<i class="far fa-times"></i>`);
   });
+
+  it('should remove the item safely.', () => {});
 });
