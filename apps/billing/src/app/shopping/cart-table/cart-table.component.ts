10f1b2f84b6d30b94dc47636f89b7ed5cc922ff7
Jorge Dacosta <jorgeMSDC@mail.com>
2020-01-10 14:34:27 +0100

    chore: add custom style for input; add fn to remove item from the cart;

--- apps/billing/src/app/shopping/cart-table/cart-table.component.ts
+++ apps/billing/src/app/shopping/cart-table/cart-table.component.ts
@@ -1,17 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { CartService } from '@businx/billing/core/cart.service';
-import { Observable, of } from 'rxjs';
 import { CartItem } from '@businx/data-models';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'businx-cart-table',
   templateUrl: './cart-table.component.html',
-  styles: []
+  styles: [`
+    .w-80 {
+      width: 80px !important;
+      }
+  `]
 })
 export class CartTableComponent implements OnInit {
 
   docs: CartItem[] = [];
   constructor(private cs: CartService) { }
+
+  remove(obj: CartItem) {
+    this.cs.remove(obj);
+  }
 
   ngOnInit() {
     this.docs = this.cs.items$;
