31b10be52e6e9e52ed6db4b4ef5d74acca417100
Jorge Dacosta <jorgeMSDC@mail.com>
2020-01-15 23:08:52 +0100

    chore: register checkout route path

--- apps/billing/src/app/shopping/shopping-routing.module.ts
+++ apps/billing/src/app/shopping/shopping-routing.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { CartComponent } from './cart/cart.component';
+import { CheckoutComponent } from './checkout/checkout.component';
 
 
 const routes: Routes = [
   {
     path: '',
     children: [
-      { path: 'cart', component: CartComponent }
+      { path: 'cart', component: CartComponent },
+      { path: 'checkout', component: CheckoutComponent }
     ]
   }
 ];
