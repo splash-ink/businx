73231889bae099ab21251979349f96a2be310a1c
Jorge Dacosta <jorgeMSDC@mail.com>
2020-01-15 12:40:46 +0100

    chore: add business logic and comment it

--- libs/invoice/src/lib/invoice.component.ts
+++ libs/invoice/src/lib/invoice.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+// import * as jsPDF from 'jspdf';
+// import html2canvas from 'html2canvas';
+import { TERMS } from './globals';
+import { Invoice } from './invoice';
 
 @Component({
   selector: 'businx-invoice',
@@ -7,7 +11,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InvoiceComponent implements OnInit {
 
+  @Input('invoice-data') doc: Invoice;
+  
+  terms = TERMS;
+  
   constructor() { }
+
+  // async exportPDF() {
+  //   const doc = new jsPDF('p', 'mm', 'a4', 2);
+  //   const canvas = await html2canvas(this.el.nativeElement, {
+  //     allowTaint: true,
+  //     useCORS: false,
+  //   });
+  //   const width = doc.internal.pageSize.getWidth();
+  //   const height = doc.internal.pageSize.getHeight() - 100;
+  //   const img = canvas.toDataURL('image/png');
+
+  //   console.log('W:' + width);
+  //   console.log('H:' + height);
+
+  //   doc.addImage(img, 'JPEG', 0, 0, width, 170);
+  //   doc.save(`SPL-${this.doc.id}.pdf`);
+
+  // }
 
   ngOnInit() {
   }
