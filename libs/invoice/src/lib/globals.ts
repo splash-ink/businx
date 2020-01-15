8513e64932acf3918d8f5940fd58b8660407f5ed
Jorge Dacosta <jorgeMSDC@mail.com>
2020-01-15 10:53:54 +0100

    chore: add policy terms

---
+++ libs/invoice/src/lib/globals.ts
@@ -0,0 +1,2 @@
+export const TERMS = 'O cliente deve tomar conhecimento sobre os itens da factura antes do pagamento. O pagamento de toda a factura deve se\
+r feito no prazo da factura, sendo que no contrário será necessário a emissão de uma nova. Não fazemos devoluções de valores.';
