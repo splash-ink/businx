a48c0a352e3ff514d2e7a75ba42a5ca42c93314c
Jorge Dacosta <jorgeMSDC@mail.com>
2020-01-16 12:46:04 +0100

    chore: remove dead code

--- libs/invoice/src/lib/invoice.component.ts
+++ libs/invoice/src/lib/invoice.component.ts
@@ -1,6 +1,4 @@
 import { Component, OnInit, Input } from '@angular/core';
-// import * as jsPDF from 'jspdf';
-// import html2canvas from 'html2canvas';
 import { TERMS } from './globals';
 import { Invoice } from './invoice';
 
@@ -16,24 +14,6 @@ export class InvoiceComponent implements OnInit {
   terms = TERMS;
   
   constructor() { }
-
-  // async exportPDF() {
-  //   const doc = new jsPDF('p', 'mm', 'a4', 2);
-  //   const canvas = await html2canvas(this.el.nativeElement, {
-  //     allowTaint: true,
-  //     useCORS: false,
-  //   });
-  //   const width = doc.internal.pageSize.getWidth();
-  //   const height = doc.internal.pageSize.getHeight() - 100;
-  //   const img = canvas.toDataURL('image/png');
-
-  //   console.log('W:' + width);
-  //   console.log('H:' + height);
-
-  //   doc.addImage(img, 'JPEG', 0, 0, width, 170);
-  //   doc.save(`SPL-${this.doc.id}.pdf`);
-
-  // }
 
   ngOnInit() {
   }
