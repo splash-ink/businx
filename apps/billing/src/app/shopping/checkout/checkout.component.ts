8da73bd047a54d064ecbf4db852bb19da006e30a
Jorge Dacosta <jorgeMSDC@mail.com>
2020-01-16 12:49:10 +0100

    chore: DOM el ref; add method to export invoice as PDF

--- apps/billing/src/app/shopping/checkout/checkout.component.ts
+++ apps/billing/src/app/shopping/checkout/checkout.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { DEMO } from '@businx/invoice';
 
 @Component({
   selector: 'businx-checkout',
@@ -7,7 +8,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CheckoutComponent implements OnInit {
 
+  @ViewChild('invoice', { static: true }) el: ElementRef;
+  dummy = DEMO;
+
   constructor() { }
+
+  async exportAsPDF() {
+    const opt = {
+      margin:       0,
+      filename:     `SPL-${this.dummy.id}.pdf`,
+      image:        { type: 'jpeg', quality: 1 },
+      html2canvas:  { scale: 1 },
+      jsPDF:        { unit: 'in', format: 'a4', orientation: 'portrait' }
+    };
+    
+    html2pdf()
+    .set(opt)
+    .from(this.el.nativeElement)
+    .save();
+  }
 
   ngOnInit() {
   }
