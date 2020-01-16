cb7bd90602da43e06b22eb04fd87b7ba09dfd9b2
Jorge Dacosta <jorgeMSDC@mail.com>
2020-01-16 15:27:02 +0100

    chore(seed): add hard coded dummy data

--- libs/invoice/src/lib/globals.ts
+++ libs/invoice/src/lib/globals.ts
@@ -20,16 +20,68 @@ export const DEMO: Invoice = {
     due_date: dueDate,
     items: [
         {
-            id: '0',
+            id: '1',
             item: {
-                id: 0,
-                service: 'My Cart Item',
-                price: 5900,
+                id: 1,
+                service: 'My Cart Item 1',
+                price: 5100,
                 description: 'Some description from service',
                 type: 'design',
                 buyInfo: false
             },
             quantity: 1,
+            urgency: false
+        },
+        {
+            id: '2',
+            item: {
+                id: 2,
+                service: 'My Cart Item 2',
+                price: 5200,
+                description: 'Some description from service',
+                type: 'design',
+                buyInfo: false
+            },
+            quantity: 2,
+            urgency: false
+        },
+        {
+            id: '3',
+            item: {
+                id: 3,
+                service: 'My Cart Item 3',
+                price: 5300,
+                description: 'Some description from service',
+                type: 'design',
+                buyInfo: false
+            },
+            quantity: 3,
+            urgency: false
+        },
+        {
+            id: '4',
+            item: {
+                id: 4,
+                service: 'My Cart Item 4',
+                price: 5400,
+                description: 'Some description from service',
+                type: 'design',
+                buyInfo: false
+            },
+            quantity: 4,
+            urgency: false
+        },
+        {
+            id: '5',
+            item: {
+                id: 5,
+                service: 'My Cart Item 5',
+                price: 5500,
+                description: 'Some description from service',
+                type: 'design',
+                buyInfo: false
+            },
+            quantity: 5,
             urgency: false
         }
     ]
