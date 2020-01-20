66c65df8a6c910ae1528d5ed74c4b6d9b3907f73
Jorge Dacosta <jorgeMSDC@mail.com>
2020-01-20 19:51:33 +0100

    chore: delete invoice model & globals from lib

--- libs/invoice/src/lib/invoice.ts
+++
@@ -1,9 +0,0 @@
-import { CartItem, Contacts } from '@businx/data-models';
-
-export interface Invoice {
-    id: string | number;
-    buyer: Contacts;
-    items: CartItem[]
-    issue_date: number;
-    due_date: Date;
-}
