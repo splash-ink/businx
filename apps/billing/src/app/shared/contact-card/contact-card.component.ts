cb238768d8c4feb9890ac12012deb91bc6aa95f5
Jorge Dacosta <jorgeMSDC@mail.com>
2020-01-21 20:35:57 +0100

    chore: generate contact-card component files

---
+++ apps/billing/src/app/shared/contact-card/contact-card.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'businx-contact-card',
+  templateUrl: './contact-card.component.html',
+  styles: []
+})
+export class ContactCardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
