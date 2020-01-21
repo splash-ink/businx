ebeeebf8b52a2b30bde07cd41fa35a4fcdc0b50d
Jorge Dacosta <jorgeMSDC@mail.com>
2020-01-21 20:39:39 +0100

    chore(linter): avoid circular dependency at same app domain.

--- apps/billing/src/app/contacts/contacts.module.ts
+++ apps/billing/src/app/contacts/contacts.module.ts
@@ -3,7 +3,7 @@ import { CommonModule } from '@angular/common';
 
 import { ContactsRoutingModule } from './contacts-routing.module';
 import { ContactsComponent } from './contacts.component';
-import { SharedModule } from '@businx/billing/shared/shared.module';
+import { SharedModule } from '../shared/shared.module';
 import { ContactCreateModalComponent } from './contact-create-modal/contact-create-modal.component';
 import { ContactDetailsComponent } from './contact-details/contact-details.component';
 import { ContactEditComponent } from './contact-edit/contact-edit.component';
