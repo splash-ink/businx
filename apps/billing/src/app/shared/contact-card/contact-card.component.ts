a2602c128e9d7d66f8b66823d55f5beee68b21ff
Jorge Dacosta <jorgeMSDC@mail.com>
2020-01-22 23:26:22 +0100

    chore: add all presentation logic

--- apps/billing/src/app/shared/contact-card/contact-card.component.ts
+++ apps/billing/src/app/shared/contact-card/contact-card.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Contacts } from '../../contacts/contacts.model';
+import { Router } from '@angular/router';
+import { StringManipulation } from '../utils/index';
 
 @Component({
   selector: 'businx-contact-card',
   templateUrl: './contact-card.component.html',
   styles: []
 })
-export class ContactCardComponent implements OnInit {
+export class ContactCardComponent {
 
-  constructor() { }
+  @Input('data') data: Contacts;
+  @Input('controls') ctrl? = true;
+  @Input('expose-cart-ops') cartOps? = false;
+  @Input('custom-function') fn?: Function;
 
-  ngOnInit() {
+  constructor(private router: Router, private strmp: StringManipulation) {}
+
+  navigateToEdit() {
+    this.router.navigate([`/contacts/${this.data.id}/edit`]);
+  }
+
+  getNameInitials(): string {
+    return this.strmp.getAbbreviation(this.data.name);
   }
 
 }
