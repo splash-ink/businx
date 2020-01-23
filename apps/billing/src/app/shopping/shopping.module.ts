fb3219e0a39b579d2b75d89c33f041e229664d9b
Jorge Dacosta <jorgeMSDC@mail.com>
2020-01-23 15:02:06 +0100

    fix: resolve broken path for cart per-deps

--- apps/billing/src/app/shopping/shopping.module.ts
+++ apps/billing/src/app/shopping/shopping.module.ts
@@ -2,18 +2,24 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { InvoiceModule } from '@businx/invoice';
 import { SharedModule } from '../shared/shared.module';
-import { CartTableComponent } from './cart-table/cart-table.component';
+import { CartTableComponent } from './cart/cart-table/cart-table.component';
 import { CartComponent } from './cart/cart.component';
 import { CartService } from './cart/cart.service';
 import { CheckoutComponent } from './checkout/checkout.component';
-import { ModalItemListComponent } from './modal-item-list/modal-item-list.component';
+import { ModalItemListComponent } from './cart/modal-item-list/modal-item-list.component';
 import { ShoppingRoutingModule } from './shopping-routing.module';
-import { SummaryComponent } from './summary/summary.component';
+import { SummaryComponent } from './cart/summary/summary.component';
 
 
 
 @NgModule({
-  declarations: [CartComponent, CartTableComponent, SummaryComponent, ModalItemListComponent, CheckoutComponent],
+  declarations: [
+    CartComponent,
+    CartTableComponent,
+    SummaryComponent,
+    ModalItemListComponent,
+    CheckoutComponent
+  ],
   imports: [
     CommonModule,
     ShoppingRoutingModule,
