a99d5e8c7a0175d8f78c0feb60938ef27ff9a245
Jorge Dacosta <jorgeMSDC@mail.com>
2020-01-24 23:19:36 +0100

    chore(cart): injecting cart-service and persisting data on localStorage

--- apps/billing/src/app/shopping/cart/summary/summary.component.ts
+++ apps/billing/src/app/shopping/cart/summary/summary.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CartService } from '../cart.service';
 
 @Component({
   selector: 'businx-summary',
@@ -7,7 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SummaryComponent implements OnInit {
 
-  constructor() { }
+  ship = this.cs.getFromLocalStorage('CART_SHIP', 0);
+  discount = this.cs.getFromLocalStorage('CART_DISCOUNT', 0);;
+
+  constructor(public cs: CartService) { }
+
+  checkDiscountLimit() {
+    if (this.discount > 30)
+      this.discount = 30
+
+    this.cs.setToLocalStorage('CART_DISCOUNT', this.discount);
+  }
 
   ngOnInit() {
   }
