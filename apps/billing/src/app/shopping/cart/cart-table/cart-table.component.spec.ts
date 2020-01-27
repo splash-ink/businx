77917abefc24710e4ea5452babad64cd9de67897
Jorge Dacosta <jorgeMSDC@mail.com>
2020-01-27 14:50:15 +0100

    fix: refactor hardcoded contact with new class model;

--- apps/billing/src/app/shopping/cart/cart-table/cart-table.component.spec.ts
+++ apps/billing/src/app/shopping/cart/cart-table/cart-table.component.spec.ts
@@ -2,27 +2,25 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { CartTableComponent } from './cart-table.component';
 import { By } from '@angular/platform-browser';
-import { CartItem } from '@businx/data-models';
+import { ICartItem } from '../cart.model';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
 
 describe('CartTableComponent', () => {
   let component: CartTableComponent;
   let fixture: ComponentFixture<CartTableComponent>;
-  const demo: CartItem = {
-    id: 1,
-    item:{
-      id: 'zyx',
+  const demo: ICartItem = new ICartItem({
+      id: 123,
       service: 'service name',
       description: 'Loren ipsum, dolet amet rusir.',
       price: 5000,
       type: 'tech',
       buyInfo: false
-    },
-    quantity: 1,
-  }
+    }, 1, false);
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ CartTableComponent ]
+      declarations: [ CartTableComponent ],
+      schemas: [ NO_ERRORS_SCHEMA ]
     })
     .compileComponents();
   }));
@@ -30,19 +28,24 @@ describe('CartTableComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(CartTableComponent);
     component = fixture.componentInstance;
+
+    component.data.push(demo);
+    component.onUpdate = null;
+    component.onDelete = null;
+
     fixture.detectChanges();
   });
 
   it('has five (5) head columns', () => {
-    const th = fixture.debugElement.queryAll(By.css('thead > tr > th'));
-    
+    const th = fixture.debugElement.queryAll(By.css('.table thead > tr > th'));
+
     expect(component).toBeTruthy();
     expect(th.length).toBe(5);
   });
 
   it('has the correct columns names', () => {
-    const th = fixture.debugElement.queryAll(By.css('thead > tr > th'));
-    
+    const th = fixture.debugElement.queryAll(By.css('.table > thead > tr > th'));
+
     expect(th[0].nativeElement.textContent).toBe('Producto');
     expect(th[1].nativeElement.textContent).toBe('Preço');
     expect(th[2].nativeElement.textContent).toBe('Quantidade');
@@ -51,16 +54,10 @@ describe('CartTableComponent', () => {
   });
 
   it('has a actions-menu that helps manage items inside the cart', () => {
-    component.docs = [demo];
-
-    fixture.detectChanges();
-
     const a = fixture.debugElement.queryAll(By.css('.actions > a'));
 
     expect(a.length).toBe(2);
     expect(a[0].nativeElement.innerHTML).toBe(`<i class="far fa-external-link-alt"></i>`);
     expect(a[1].nativeElement.innerHTML).toBe(`<i class="far fa-times"></i>`);
   });
-
-  it('should remove the item safely.', () => {});
 });
