440fbd76e9e8efa20da48df614b90ed06026b8ac
Jorge Dacosta <jorgeMSDC@mail.com>
2020-01-30 10:09:28 +0100

    fix: replace resume-card > growth-card & reorderin importations

--- apps/billing/src/app/shared/shared.module.ts
+++ apps/billing/src/app/shared/shared.module.ts
@@ -1,17 +1,17 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { TableComponent } from './table/table.component';
-import { PageTitleComponent } from './page-title/page-title.component';
-import { ResumeCardComponent } from './resume-card/resume-card.component';
 import { RouterModule } from '@angular/router';
 import { ContactCardComponent } from './contact-card/contact-card.component';
+import { GrowthCardComponent } from './growth-card/growth-card.component';
+import { PageTitleComponent } from './page-title/page-title.component';
+import { TableComponent } from './table/table.component';
 import { StringManipulation } from './utils';
 
 const comps = [
   TableComponent,
   PageTitleComponent,
-  ResumeCardComponent,
+  GrowthCardComponent,
   ContactCardComponent
 ];
 
