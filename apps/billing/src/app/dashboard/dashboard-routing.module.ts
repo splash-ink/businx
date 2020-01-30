e84f654ebbf87c492d2467a12cb8ffc21fef214f
Jorge Dacosta <jorgeMSDC@mail.com>
2020-01-30 11:32:14 +0100

    core: register dashboard-page default route path

--- apps/billing/src/app/dashboard/dashboard-routing.module.ts
+++ apps/billing/src/app/dashboard/dashboard-routing.module.ts
@@ -1,8 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DashboardComponent } from './dashboard.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    children: [
+      {
+        path: '',
+        pathMatch: 'full',
+        component: DashboardComponent
+      }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
