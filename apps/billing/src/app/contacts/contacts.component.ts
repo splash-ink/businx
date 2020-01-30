54835fa382363d755fed7787c4935847b38fcbc9
Jorge Dacosta <jorgeMSDC@mail.com>
2020-01-30 10:42:25 +0100

    fix: remove unnecessary imports

--- apps/billing/src/app/contacts/contacts.component.ts
+++ apps/billing/src/app/contacts/contacts.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { ResumeCard } from '@businx/billing/shared/resume-card/resume-card';
 import { Table } from '@businx/billing/shared/table/table';
 import { CONTACTS_TABLE_COLUMNS } from '@businx/data-models';
 import { Contacts } from './contacts.model';
@@ -12,66 +11,14 @@ import { Contacts } from './contacts.model';
 export class ContactsComponent implements OnInit {
   contactsTbl: Table;
 
-  data: Contacts [] = [
-    {
-      id: 1,
-      name: 'Georgia Dacosta',
-      company: 'Spatial LLC',
-      email: 'georgia@spatialx3c.io',
-      phone: '+190595962'
-    },
-    {
-      id: 2,
-      name: 'Anisa Makayla Dacosta',
-      company: 'Google Stadia',
-      email: 'makayla.a@stadia.google.com',
-      phone: 951826952
-    }
-  ]
-  contactsCrd: ResumeCard [] = [
-    {
-      title: 'Novos contactos',
-      isCurrency: false,
-      icon: {
-        bg: 'bg-gradient-danger',
-        class: 'fa-users'
-      },
-      values: {
-        previews: 100000,
-        current: 20000
-      }
-    },
-    {
-      title: 'Vendas totais',
-      isCurrency: true,
-      icon: {
-        bg: 'bg-gradient-success',
-        class: 'fa-hand-holding-usd'
-      },
-      values: {
-        previews: 370000,
-        current: 872000
-      }
-    },
-    {
-      title: 'Encomendas',
-      isCurrency: false,
-      icon: {
-        bg: 'bg-gradient-info',
-        class: 'fa-tag'
-      },
-      values: {
-        previews: 72,
-        current: 72
-      }
-    }
-  ];
+  contacts$: Contacts [] = [];
+
   constructor() { }
 
   ngOnInit() {
     this.contactsTbl  = {
       columns: CONTACTS_TABLE_COLUMNS,
-      dataset: this.data,
+      dataset: this.contacts$,
       collection: 'contacts',
       title: 'Clientes'
     }
