8c179270efb5ec99dd92d6e3f52bed97b4a36d30
Jorge Dacosta <jorgeMSDC@mail.com>
2020-01-31 11:48:46 +0100

    chore(@billing/shared): generating apex-line(chart) comp files

---
+++ apps/billing/src/app/shared/apex-line/apex-line.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'businx-apex-line',
+  templateUrl: './apex-line.component.html',
+  styleUrls: ['./apex-line.component.css']
+})
+export class ApexLineComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
