53c7c7bb9da57baa33e0649e27e569750a86878f
Jorge Dacosta <jorgeMSDC@mail.com>
2020-02-06 23:09:13 +0100

    chore: setup nest on cloud functions

--- apps/functions/src/main.ts
+++ apps/functions/src/main.ts
@@ -1,20 +1,22 @@
-/**
- * This is not a production server yet!
- * This is only a minimal backend to get started.
- */
-
 import { NestFactory } from '@nestjs/core';
-
+import { ExpressAdapter } from '@nestjs/platform-express';
 import { AppModule } from './app/app.module';
+import * as express from 'express';
+import * as functions from 'firebase-functions';
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  const globalPrefix = 'api';
-  app.setGlobalPrefix(globalPrefix);
-  const port = process.env.port || 3333;
-  await app.listen(port, () => {
-    console.log('Listening at http://localhost:' + port + '/' + globalPrefix);
-  });
-}
+const server = express();
 
-bootstrap();
+export const createNestServer = async (expressInstance) => {
+  const app = await NestFactory.create(
+    AppModule,
+    new ExpressAdapter(expressInstance),
+  );
+
+  return app.init();
+};
+
+createNestServer(server)
+    .then(v => console.log('Nest Ready'))
+    .catch(err => console.error('Nest broken', err));
+
+export const api = functions.https.onRequest(server);
