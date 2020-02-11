a04988106210aa33433e823a2205cdf8163695b4
Jorge Dacosta <jorgedacostaza@gmail.com>
2020-02-11 16:53:24 +0100

    fix: cart summary persists erased data (#42)
    * fix(cart): managing summary state with I/O
    
    * fix: misc changes added
    
    * fix(cart-svc): add method to remove a localstorage item
    
    * fix(summary): bind data incoming from input fields
    
    * fix: set inputs ops;
    outputing ship & discount values to be handled by parent comp (cart)

--- apps/billing/src/app/shopping/cart/cart.component.ts
+++ apps/billing/src/app/shopping/cart/cart.component.ts
@@ -3,6 +3,11 @@ import { CartService } from './cart.service';
 import { Router } from '@angular/router';
 import { ICartItem } from './cart.model';
 
+export const LocalKeys = [
+  'CART_SHIP',
+  'CART_DISCOUNT'
+];
+
 @Component({
   selector: 'businx-cart',
   templateUrl: './cart.component.html',
@@ -19,6 +24,12 @@ export class CartComponent implements OnInit {
     this.route.navigate(['/shopping/checkout'])
   }
 
+  clearCartData() {
+    this.cs.unsetCartItems();
+    this.syncToLocalStorage(LocalKeys[0], 0);
+    this.syncToLocalStorage(LocalKeys[1], 0);
+  }
+
   onDelete($event: ICartItem) {
     this.cs.unsetCartItem($event);
   }
@@ -27,6 +38,33 @@ export class CartComponent implements OnInit {
     const [ item, newVal] = $event;
 
     this.cs.updateCartItemQty(item, newVal);
+  }
+
+  syncToLocalStorage(key: string, $event: number) {
+    if (typeof $event !== 'number' || undefined)
+      return;
+
+    this.cs.setToLocalStorage(key, $event);
+  }
+
+  getShip() {
+    return this.cs.getFromLocalStorage(LocalKeys[0], 0);
+  }
+
+  getDiscount() {
+    return this.cs.getFromLocalStorage(LocalKeys[1], 0);
+  }
+
+  getTax(): number {
+    return this.cs.getTax();
+  }
+
+  getSubtotal(): number {
+    return this.cs.getSubtotal();
+  }
+
+  getTotal(): number {
+    return this.cs.getTotal(this.getShip(), this.getDiscount());
   }
 
   ngOnInit() {
