f1af61276047a0cad0825b1014b7a4175f22c187
Jorge Dacosta <jorgeMSDC@mail.com>
2020-02-14 17:14:39 +0100

    chore(@billing/contacts): adding contact preview type

--- apps/billing/src/app/contacts/contact.model.ts
+++ apps/billing/src/app/contacts/contact.model.ts
@@ -1,14 +1,15 @@
 import { IInvoice } from '@businx/billing/shopping/invoice.model';
-
 export interface Contact {
-    id: string | number;
-    accountType?: string;
-    greet?: string;
-    name: string;
-    company?: string;
-    email?: string;
-    phone: string | number;
-    nif?: string;
-    coin?: string;
-    invoices?: IInvoice [];
+  id: string | number;
+  accountType?: string;
+  greet?: string;
+  name: string;
+  company?: string;
+  email?: string;
+  phone: string | number;
+  nif?: string;
+  coin?: string;
+  invoices?: IInvoice [];
 }
+
+export type contactPreview = Omit<Contact,'accountType'|'greet'|'nif'|'coin'|'invoices'>
