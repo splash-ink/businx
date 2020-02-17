c61e41274198280d1eadafdeb40e208bdbbf5eae
Jorge Dacosta <jorgeMSDC@mail.com>
2020-02-18 00:04:30 +0100

    feat(@billing/contacts): managing contacts ops:
    add & remove contact from table logic added;

--- apps/billing/src/app/contacts/contacts.component.ts
+++ apps/billing/src/app/contacts/contacts.component.ts
@@ -1,27 +1,74 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Table } from '@businx/billing/shared/table/table';
 import { CONTACTS_TABLE_COLUMNS } from '@businx/data-models';
-import { Contact } from './contact.model';
+import { contactPreview } from './contact.model';
+import { FirestoreDataService } from '@businx/firestore-data-service';
+import { Subscription } from 'rxjs';
+
 
 @Component({
   selector: 'businx-contacts',
   templateUrl: './contacts.component.html',
   styleUrls: ['./contacts.component.css']
 })
-export class ContactsComponent implements OnInit {
+export class ContactsComponent implements OnInit, OnDestroy {
+
+  ref = 'companies/splashink/contacts';
+  subscription: Subscription;
   contactsTbl: Table;
+  contacts$: contactPreview [] = [];
 
-  contacts$: Contact [] = [];
+  constructor(private readonly fsds: FirestoreDataService) { }
 
-  constructor() { }
+  onDelete($event) {
+    this.fsds.delete(`${this.ref}/${$event}`);
+    this.contactsTbl.dataset.forEach((doc: contactPreview, idx) => {
+      if (doc.id == $event) {
+        this.contactsTbl.dataset.splice(idx, 1);
+      }
+    });
+
+    this.sync();
+  }
+
+  sync() {
+    this.subscription = this.fsds.findAllWithIds$<contactPreview>(this.ref)
+    .subscribe((docs) => {
+      docs.map((payload) => {
+        const contact: contactPreview = {
+          id: payload.id,
+          name: payload.name,
+          company: payload.company,
+          email: payload.email,
+          phone: payload.phone
+        }
+
+        if(this.contacts$.length > 0) {
+          this.contacts$.forEach((doc, idx) => {
+            if(doc.id == payload.id) {
+              this.contacts$.splice(idx, 1);
+            }
+          });
+        }
+
+        this.contacts$.push(contact);
+      })
+    });
+  }
 
   ngOnInit() {
-    this.contactsTbl  = {
+    this.sync();
+
+    this.contactsTbl = {
       columns: CONTACTS_TABLE_COLUMNS,
       dataset: this.contacts$,
       collection: 'contacts',
       title: 'Clientes'
     }
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
   }
 
 }
