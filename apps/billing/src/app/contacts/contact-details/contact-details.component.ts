23f6a15767ebee00794bcc250aa60f585a19cf43
Jorge Dacosta <jorgeMSDC@mail.com>
2020-02-18 11:41:28 +0100

    feat(@billing/contacts): injecting shared utils

--- apps/billing/src/app/contacts/contact-details/contact-details.component.ts
+++ apps/billing/src/app/contacts/contact-details/contact-details.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Table } from '@businx/billing/shared/table/table';
 import { Contact } from '@businx/billing/contacts';
 import { FirestoreDataService } from '@businx/firestore-data-service';
+import { Misc } from '@businx/billing/shared/utils';
 @Component({
   selector: 'businx-contact-details',
   templateUrl: './contact-details.component.html',
@@ -13,7 +14,10 @@ export class ContactDetailsComponent implements OnInit {
   invoicesTbl: Table;
   contactDemo: Contact;
 
-  constructor(private readonly fds: FirestoreDataService) { }
+  constructor(
+    private readonly fds: FirestoreDataService,
+    private readonly utils: Misc
+    ) { }
 
   ngOnInit() {
     this.invoicesTbl = {
