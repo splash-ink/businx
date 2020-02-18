0618c5fb21622cf2d3194c804853df7e378061ea
Jorge Dacosta <jorgeMSDC@mail.com>
2020-02-18 11:37:56 +0100

    refactor: remove unnecessary code:
    * remove hard coded contact obj;
    * fn that get the ID from URL;

--- apps/billing/src/app/contacts/contact-details/contact-details.component.ts
+++ apps/billing/src/app/contacts/contact-details/contact-details.component.ts
@@ -1,8 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Table } from '@businx/billing/shared/table/table';
-import { ActivatedRoute } from '@angular/router';
-import { switchMap } from 'rxjs/operators';
-import { Contact } from '../contact.model';
+import { Contact } from '@businx/billing/contacts';
 @Component({
   selector: 'businx-contact-details',
   templateUrl: './contact-details.component.html',
@@ -10,26 +8,13 @@ import { Contact } from '../contact.model';
 })
 export class ContactDetailsComponent implements OnInit {
 
-  invoicesTbl: Table;
   id;
+  invoicesTbl: Table;
+  contactDemo: Contact;
 
-  contactDemo: Contact = {
-    id: 1,
-    name: 'Georgia Dacosta',
-    company: 'Spatial LLC',
-    email: 'georgia@spatialx3c.io',
-    phone: '+190595962'
-  }
-
-  constructor(private route: ActivatedRoute) { }
+  constructor() { }
 
   ngOnInit() {
-    this.route
-    .paramMap
-    .pipe(
-      switchMap(params => this.id = params.get('id'))
-    );
-
     this.invoicesTbl = {
       columns: ['Fatura', 'Ordem', 'Valor', 'Impostos'],
       dataset: [],
