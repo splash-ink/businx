ebff97ab5a71010973c688029b418bd73f81daf1
Jorge Dacosta <jorgeMSDC@mail.com>
2020-02-19 16:19:10 +0100

    chore(@billing/items): adding helpers

--- apps/billing/src/app/items/item-edit/item-edit.component.ts
+++ apps/billing/src/app/items/item-edit/item-edit.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Item } from '@businx/data-models';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'businx-item-edit',
@@ -6,6 +8,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./item-edit.component.css']
 })
 export class ItemEditComponent implements OnInit {
+
+  id;
+  doc$: Observable<Item>;
 
   constructor() { }
 
