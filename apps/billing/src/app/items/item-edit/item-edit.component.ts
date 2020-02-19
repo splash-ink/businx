1fdee1a4e828fcef2eb012251a3226ba3642a473
Jorge Dacosta <jorgeMSDC@mail.com>
2020-02-19 16:20:23 +0100

    chore(@billing/items): injecting data & router services

--- apps/billing/src/app/items/item-edit/item-edit.component.ts
+++ apps/billing/src/app/items/item-edit/item-edit.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { Item } from '@businx/data-models';
+import { FirestoreDataService } from '@businx/firestore-data-service';
 import { Observable } from 'rxjs';
 
 @Component({
@@ -12,7 +14,10 @@ export class ItemEditComponent implements OnInit {
   id;
   doc$: Observable<Item>;
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private readonly fds: FirestoreDataService
+  ) { }
 
   ngOnInit() {
   }
