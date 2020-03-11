4b55f88ac6350b3f1c5a57f5b0d6484626373719
Jorge Dacosta <jorgeMSDC@mail.com>
2020-03-11 12:46:29 +0100

    chore(billing/orders): register orders route path

--- apps/billing/src/app/orders/orders-routing.module.ts
+++ apps/billing/src/app/orders/orders-routing.module.ts
@@ -1,8 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { OrdersComponent } from './orders.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    children: [
+      {
+        path: '',
+        pathMatch: 'full',
+        component: OrdersComponent
+      }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
