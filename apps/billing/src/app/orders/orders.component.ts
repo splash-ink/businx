b9718875ec8c11fad0ae85f25ac187789e5f0780
Jorge Dacosta <jorgeMSDC@mail.com>
2020-03-11 12:43:03 +0100

    chore: generate orders comp files

---
+++ apps/billing/src/app/orders/orders.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'businx-orders',
+  templateUrl: './orders.component.html',
+  styles: []
+})
+export class OrdersComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
