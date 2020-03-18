82f3a5a4482f81408ea193d657e536312b683deb
Jorge Dacosta <jorgeMSDC@mail.com>
2020-03-18 22:57:48 +0100

    chore(billing/orders): gen order-list comp files & regon mod

--- apps/billing/src/app/orders/orders.module.ts
+++ apps/billing/src/app/orders/orders.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 
 import { OrdersRoutingModule } from './orders-routing.module';
 import { OrdersComponent } from './orders.component';
+import { OrderListComponent } from './order-list/order-list.component';
 
 
 @NgModule({
-  declarations: [OrdersComponent],
+  declarations: [OrdersComponent, OrderListComponent],
   imports: [
     CommonModule,
     OrdersRoutingModule
