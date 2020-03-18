3b9fb69ea61da5e58e97a9d09fca0c0466fbafbf
Jorge Dacosta <jorgeMSDC@mail.com>
2020-03-18 23:01:39 +0100

    chore(billing/orders): set parent comp & list route

--- apps/billing/src/app/orders/orders-routing.module.ts
+++ apps/billing/src/app/orders/orders-routing.module.ts
@@ -1,16 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { OrdersComponent } from './orders.component';
+import { OrderListComponent } from './order-list/order-list.component';
 
 
 const routes: Routes = [
   {
     path: '',
+    component: OrdersComponent,
     children: [
       {
-        path: '',
+        path: ':id',
         pathMatch: 'full',
-        component: OrdersComponent
+        component: OrderListComponent
       }
     ]
   }
