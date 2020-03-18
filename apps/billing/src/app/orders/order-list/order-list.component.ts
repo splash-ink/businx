a871e5678839e80a8c037f7204ef2af404343367
Jorge Dacosta <jorgeMSDC@mail.com>
2020-03-18 23:03:58 +0100

    chore(billing/orders): injecting ActivatedRoute

--- apps/billing/src/app/orders/order-list/order-list.component.ts
+++ apps/billing/src/app/orders/order-list/order-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'businx-order-list',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OrderListComponent implements OnInit {
 
-  constructor() { }
+  constructor(private readonly route: ActivatedRoute) { }
 
   ngOnInit() {
   }
