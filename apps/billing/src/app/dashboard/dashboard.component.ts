845d5bd5dbcf2400d65da9fb2314c9c1c4724cff
Jorge Dacosta <jorgeMSDC@mail.com>
2020-03-24 17:28:52 +0100

    chore(billing/dashboard): misc changes added.
    - Add css classes to manage invoice list state;
    - Inject firestore data service;
    - Fetch the 4 last invoices created;

--- apps/billing/src/app/dashboard/dashboard.component.ts
+++ apps/billing/src/app/dashboard/dashboard.component.ts
@@ -1,21 +1,43 @@
 import { Component, OnInit } from '@angular/core';
 import { GrowthCardModel } from '../shared/growth-card/growth-card.model';
 import { growthCardsDemo } from '../shared/globals';
+import { Observable } from 'rxjs';
+import { Order } from '@businx/data-models';
+import { FirestoreDataService } from '@businx/firestore-data-service';
 
 @Component({
   selector: 'businx-dashboard',
   templateUrl: './dashboard.component.html',
-  styles: []
+  styles: [
+    `
+    .empty-invoice-list {
+      position: absolute;
+      left: 20%;
+      font-size: 1rem;
+    }
+
+    @media screen and (max-width: 375px) {
+      .empty-invoice-list {
+        left: .5rem;
+        padding: 0 1rem;
+        text-align: center;
+      }
+    }
+    `
+  ]
 })
 export class DashboardComponent implements OnInit {
 
   growthCards$: GrowthCardModel [] = [];
+  orders$: Observable<Order []>;
+  baseRef = 'companies/splashink';
 
-  constructor() {
+  constructor(private readonly fds: FirestoreDataService) {
     this.growthCards$ = growthCardsDemo;
   }
 
   ngOnInit() {
+    this.orders$ = this.fds.findAllWithIds$<Order>(this.baseRef + '/invoices', query => query.orderBy('createdAt', 'desc').limit(4));
   }
 
 }
