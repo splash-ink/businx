69c5e76b4d1044d200f35dacbedd6e3240f5dcb2
Jorge Dacosta <jorgeMSDC@mail.com>
2020-03-24 11:28:44 +0100

    feat(billing/orders): add fetch & sort fn

--- apps/billing/src/app/orders/orders.component.ts
+++ apps/billing/src/app/orders/orders.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FirestoreDataService } from '@businx/firestore-data-service';
+import { Order } from '@businx/data-models';
+import { Observable } from 'rxjs';
+
 
 @Component({
   selector: 'businx-orders',
@@ -8,10 +12,28 @@ import { Component, OnInit } from '@angular/core';
 export class OrdersComponent implements OnInit {
 
   sort = '';
+  ref = 'companies/splashink/invoices';
+  docs$: Observable<Order []>;
 
-  constructor() { }
+  constructor(private readonly fds: FirestoreDataService) { }
+
+  onSort() {
+    this.docs$ = this.fetch();
+  }
+
+  fetch() {
+    if (this.sort === '' || this.sort === undefined) {
+      return this.fds.findAllWithIds$<Order>(this.ref);
+    }
+
+    return this.fds.findAllWithIds$<Order>(
+      this.ref,
+      ref => ref.orderBy('createdAt', 'desc').where(`status.${this.sort}`, '==', true)
+    );
+  }
 
   ngOnInit() {
+    this.docs$ = this.fetch();
   }
 
 }
