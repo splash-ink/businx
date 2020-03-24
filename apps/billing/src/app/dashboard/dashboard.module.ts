c800a3e8db672862c546b9e10b33f3c27d07fb4a
Jorge Dacosta <jorgeMSDC@mail.com>
2020-03-24 16:32:48 +0100

    chore(billing/dashboard): register invoice-card

--- apps/billing/src/app/dashboard/dashboard.module.ts
+++ apps/billing/src/app/dashboard/dashboard.module.ts
@@ -4,10 +4,11 @@ import { CommonModule } from '@angular/common';
 import { DashboardRoutingModule } from './dashboard-routing.module';
 import { DashboardComponent } from './dashboard.component';
 import { SharedModule } from '../shared/shared.module';
+import { InvoiceCardComponent } from './invoice-card/invoice-card.component';
 
 
 @NgModule({
-  declarations: [DashboardComponent],
+  declarations: [DashboardComponent, InvoiceCardComponent],
   imports: [
     CommonModule,
     DashboardRoutingModule,
