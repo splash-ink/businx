71ac212a7e1ce553c5ebe15cee08d5ecaa796cb4
Jorge Dacosta <jorgeMSDC@mail.com>
2020-03-24 10:56:54 +0100

    chore(billing/order): add input to manage data

--- apps/billing/src/app/orders/order-card/order-card.component.ts
+++ apps/billing/src/app/orders/order-card/order-card.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Order } from '@businx/data-models';
 
 @Component({
   selector: 'businx-order-card',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./order-card.component.css']
 })
 export class OrderCardComponent implements OnInit {
+
+  @Input() order: Omit<Order, 'shopItems' | 'date' | 'ship' | 'discounts' | 'tax' | 'subtotal'>;
 
   constructor() { }
 
